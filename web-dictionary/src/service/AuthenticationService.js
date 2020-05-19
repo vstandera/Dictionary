db492d30bdf449b4fc3d3800b64290bd470eb0c3
vstandera <vstandera@gmail.com>
2020-05-19 14:18:05 +0200

    FE modify

--- web-dictionary/src/service/AuthenticationService.js
+++ web-dictionary/src/service/AuthenticationService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_URL = 'http://localhost:8080'
-const API_URL = 'http://3.136.156.3:3000'
+const API_URL = 'http://3.136.156.3:8080'
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
